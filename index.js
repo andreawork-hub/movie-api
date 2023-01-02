2ca5eadfd1d1e398b94a4c86677f91ba0f3ea360
andreawork-hub <andrea.vitova@hotmail.com>
2023-01-02 12:24:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
