813e36229b376532bae55cb49b3875e4a8566c05
andreawork-hub <andrea.vitova@hotmail.com>
2023-01-02 02:44:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 const cors = require('cors');
 // app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+//let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 app.use(cors({
     origin: (origin, callback) => {
